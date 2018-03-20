7c01195df0e032bbb2ea34189e317b15475a21ea
jboothe <jeff.boothe@reurgency.com>
2018-03-20 10:57:10 -0400

    feat(resolvers): changes resolver merge to auto using `.resolvers.` naming convention

--- src/resolvers/index.ts
+++ src/resolvers/index.ts
@@ -1,10 +1,13 @@
-import { mergeResolvers } from "merge-graphql-schemas";
+import * as path from "path";
+import { mergeResolvers, fileLoader } from "merge-graphql-schemas";
 import userResolver from "./user.resolvers";
 import simpleResolver from "./simple.resolvers";
 
-const resolversArray = [userResolver, simpleResolver];
+// const resolversArray = [userResolver, simpleResolver];
+
+const resolversArray = fileLoader(path.join(__dirname, "./**/*.resolvers.*"));
 
 const resolvers = mergeResolvers(resolversArray);
-// console.log("resolvers", resolvers);
+console.log("resolvers", resolvers);
 
 export default resolvers;
